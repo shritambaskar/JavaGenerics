2568b7246f2f61a1e09ffeb205a35441feac26a0
DELL <DELL@DESKTOP-GRB1F1E>
2023-03-19 13:11:54 +0530

    new code add in vishal class

--- src/com/generics/demo/controller/VishalClass.java
+++ src/com/generics/demo/controller/VishalClass.java
@@ -3,7 +3,8 @@ package com.generics.demo.controller;
 public class VishalClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Ststem.out.println("this msg by vishal");
 
 	}
 
